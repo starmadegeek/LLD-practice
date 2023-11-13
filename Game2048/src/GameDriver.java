e0c390b43af208cf8ff81bea9c244e04d80f664a
Nivyanth Sai Chitturi <nivyanth.developer@gmail.com>
2023-11-13 22:16:08 +0530

    2048 game initialising

---
+++ Game2048/src/GameDriver.java
@@ -0,0 +1,9 @@
+public class GameDriver {
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm 2048!");
+
+        Game game = new Game();
+        game.start();
+
+    }
+}
